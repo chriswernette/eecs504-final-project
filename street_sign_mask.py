9cdb4a5518189e7b1c08e256c2a6788f2c4b41d6
amcrean <amcrean@github.com>
2018-12-02 01:52:59 -0500

    fixed color ranges

--- street_sign_mask.py
+++ street_sign_mask.py
@@ -1,22 +1,22 @@
 import cv2
 import numpy as np
-
+#img should be RGB
 def street_sign_mask(img):
     #Convert img to HSV colour
-    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 
     #Create masks for traffic signs - Official RGB Defined below
     #Green - 0,153, 0; Blue - 0,0,255; 
     #Orange - 255,102,0; Yellow - 255,204,0
     #Define range for each color
-    green_lower_limit = np.array([85, 255, 100]) #RGB - 0/100/0
-    green_upper_limit = np.array([85,255, 180]) #RGB - 0/180/0
-    blue_lower_limit = np.array([167, 255, 255]) #RGB - 0/20/255
-    blue_upper_limit = np.array([170, 255, 150]) #RGB - 0/0/150
-    orange_lower_limit = np.array([25,255,255]) #RGB - 255/150/0
-    orange_upper_limit = np.array([11, 255, 255]) #RGB - 255/65/0
-    yellow_lower_limit = np.array([18,94,140])
-    yellow_upper_limit = np.array([58,255,255])
+    green_lower_limit = np.array([0, 255, 80]) 
+    green_upper_limit = np.array([100,255, 120]) 
+    blue_lower_limit = np.array([100, 150, 100]) 
+    blue_upper_limit = np.array([157, 255, 255]) 
+    orange_lower_limit = np.array([0,150,230])
+    orange_upper_limit = np.array([15, 255, 255])
+    yellow_lower_limit = np.array([0,150,140])
+    yellow_upper_limit = np.array([100,255,255])
 
     mask_green = cv2.inRange(hsv_img, green_lower_limit, green_upper_limit)
     mask_blue = cv2.inRange(hsv_img, blue_lower_limit, blue_upper_limit)
