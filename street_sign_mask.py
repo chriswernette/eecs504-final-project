7d05774b13596f1769ac7b5c47db8d26a80ca638
amcrean <amcrean@github.com>
2018-12-02 02:21:37 -0500

    combined color masks

--- street_sign_mask.py
+++ street_sign_mask.py
@@ -9,7 +9,7 @@ def street_sign_mask(img):
     #Green - 0,153, 0; Blue - 0,0,255; 
     #Orange - 255,102,0; Yellow - 255,204,0
     #Define range for each color
-    green_lower_limit = np.array([0, 200, 80]) 
+    green_lower_limit = np.array([50, 100, 80]) 
     green_upper_limit = np.array([100,255, 120]) 
     blue_lower_limit = np.array([100, 150, 100]) 
     blue_upper_limit = np.array([157, 255, 255]) 
@@ -22,5 +22,9 @@ def street_sign_mask(img):
     mask_blue = cv2.inRange(hsv_img, blue_lower_limit, blue_upper_limit)
     mask_orange = cv2.inRange(hsv_img, orange_lower_limit, orange_upper_limit)
     mask_yellow = cv2.inRange(hsv_img, yellow_lower_limit, yellow_upper_limit)
-
-    return mask_green, mask_blue, mask_orange, mask_yellow
+    #Combine Masks
+    mask_tmp1 = cv2.bitwise_or(mask_green,mask_blue)
+    mask_tmp2 = cv2.bitwise_or(mask_orange,mask_yellow)
+    mask = cv2.bitwise_or(mask_tmp1, mask_tmp2)
+    
+    return mask
