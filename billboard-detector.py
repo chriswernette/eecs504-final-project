22e1361feec661cc9638dda5c60ac82f5a5f6605
Chris Wernette <chriswer@umich.edu>
2018-12-02 12:55:59 -0500

    tidied up the billboard-detector.py wrapper function and added comments to make more clear

--- billboard-detector.py
+++ billboard-detector.py
@@ -9,31 +9,31 @@ import find_intersections as fi
 from preprocessing import preprocess_data
 from cluster_corners import cluster_corners
 
-#read in image
+#read in image, this is where you specify the jpg you want to read in
 img_location = "data/real-billboard.jpg"
 
+'''calls preprocess script to crop the image to the upper right hand quadrant,
+grayscale and blur the crop, find Canny edges and Hough lines from the cropped 
+grayscale. In the future, I'll add inputs so you can specify where the function
+looks for billboards in the image. Right now it just looks in the upper right'''
 gray_cropped, img_cropped, edges, lines = preprocess_data(img_location)
 
-#plt.imshow(blur)
-#plt.show()
-
+#show what the gray blurred image looks like
 plt.imshow(gray_cropped,cmap='gray')
 plt.title('Grayscale image')
 plt.show()
-#plt.imshow(gray,cmap='gray')
-#plt.show()
 
+#show what the Canny edges look like
 plt.imshow(edges,cmap='gray')
 plt.title('Canny edges')
 plt.show() 
 
-#print("Number of lines in image: " + str(len(lines)))
-
-#put Hough lines on the cropped image
+#put Hough lines on the cropped image one at a time just to visualize
 for line in lines:
     x1, y1, x2, y2 = line[0]
     cv2.line(img_cropped, (x1, y1), (x2, y2), (0, 255, 0), 3)
 
+#convert from BGR (openCV format) to RGB (matplotlib format) and show the lines
 img_RGB = cv2.cvtColor(img_cropped, cv2.COLOR_BGR2RGB)
 plt.imshow(img_RGB)
 plt.title("cropped image with hough lines")
@@ -42,66 +42,51 @@ plt.show()
 #find intersections of Hough lines
 intersections = fi.segmented_intersections(lines)
 
-#print(intersections.shape)
-#print(intersections)
-
+#get (x,y) of the intersections so that we can plot them on top of image
 x = intersections[:,0]
 y = intersections[:,1]
 
+#show the image w/lines and then put a scatter plot of the intersections on top
 plt.imshow(img_RGB)
 plt.scatter(x, y, c='r', s=40)
 plt.title('Hough line intersections laid on top of Hough lines')
 plt.show()
 
-
-#apply clustering algorithm
+#find the clusters of the intersections - basically reduce the number of points
 labels, cluster_centers = cluster_corners(intersections)
 
+#get (x,y) of the cluster centers so we can plot those on top of the image next
 x = cluster_centers[:,0]
 y = cluster_centers[:,1]
 
-#print('x,y locations of centroids')
-#print(cluster_centers)
-
+#plot the cluster locations as a scatter plot on top of the lines image
 plt.imshow(img_RGB)
 plt.scatter(x, y, c='r', s=40)
 plt.title('Cluster Centroids laid on top of Hough lines')
 plt.show()
 
-
-#set up binary image of just points for the contour detection algorithm/convex Hull
+#need to convert cluster locations to integer so we're at valid pixel locations
 clust_int = np.int0(cluster_centers)
-#print(clust_int)
 
-test_img = np.zeros_like(gray_cropped)
-#print(test_img.shape)
-for i in range(clust_int.shape[0]):
-    x,y = clust_int[i]
-    test_img[y,x] = 1
-
-plt.imshow(test_img)
-plt.show()
-
-print(clust_int.shape)
-
+'''compute the distance from the upper left hand corner of the image (0,0) and
+use that to sort. The upper left hand corner should have the least distance and
+the lower right should have the largest.'''
 dist = np.linalg.norm(clust_int,axis=1).reshape(clust_int.shape[0],1)
-print(dist)
+#print(dist)
 idx = np.argsort(dist,axis=0)
-print(idx)
-# clust_add_dist = np.hstack((clust_int,dist))
-# print(clust_add_dist.shape)
-# print(clust_add_dist)
-# idx = np.argsort(clust_add_dist,axis=0)
-# dist_sorted = idx[:,2]
-# print(dist_sorted)
-
+#print(idx)
 clust_dist_sorted = clust_int[idx]
 print(clust_dist_sorted)
-#print(clust_dist_sorted)
 
-# cv2.imshow('draw contrours',im2)
-# cv2.waitKey(0)
-
+'''@TODO Peter is working on a function that will order these correctly for
+drawing a polygon. Basically the issue is the cluster points go down the left
+hand side of the billboard and then cut across to the upper right instead of the 
+lower right hand corner because the distance to the point (0,0) is less. But,
+for the polynomial mask function to work they need to be in the order that you
+would draw a box around the mask. So, Peter is working on a function that will
+get that correct order by finding the distance between each point, and reversing
+the array when we are about to go from bottom left corner -> upper right corner
+so it goes from bottom left corner -> bottom right corner'''
 
 #defunct testing code beyond this line
 ################################################################################
@@ -116,4 +101,17 @@ print(clust_dist_sorted)
 # plt.imshow(img_RGB)
 # plt.scatter(x,y,c='r',s=40)
 # plt.title('Corners from cv2.goodFeaturesToTrack laid on top of hough lines')
+# plt.show()
+
+################################################################################
+#this was trying out some findContours/convexHull code that didn't work
+#set up binary image of just points for the contour detection algorithm/convex Hull
+#print(clust_int)
+
+# test_img = np.zeros_like(gray_cropped)
+# for i in range(clust_int.shape[0]):
+#     x,y = clust_int[i]
+#     test_img[y,x] = 1
+
+# plt.imshow(test_img)
 # plt.show()
