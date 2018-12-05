bd7570232c96566f635ae24d21207040d53b4a5b
moefarhat <mwi@umich.edu>
2018-12-04 21:48:31 -0500

    adding img to crop save function

---
+++ img_to_imgcrop.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os 
+
+#help function which crops images and saves the new image in same directory
+img_location = 'data/Test_Images/_020_first_15/_021_first_15/frame12.jpg'
+x1 = 20
+x2 = 500
+y1 = 40
+y2 = 700
+
+img = cv2.imread(img_location)
+imgcrop = img[x1:x2,y1:y2]
+newname = 'croped_'+ os.path.basename(img_location)
+newnewname = os.path.dirname(img_location) + '/' + newname
+#saves to directory
+cv2.imwrite(newnewname,imgcrop)
