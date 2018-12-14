f765b6b048dcf84ad3f7eb7bc31ea34ac638c620
pmpaquet <pmpaquet@umich.edu>
2018-12-14 16:06:45 -0500

    reject.py placeholder

---
+++ reject.py
@@ -0,0 +1,18 @@
+import numpy as np
+import os
+import cv2
+import matplotlib.pyplot as plt
+import eta.core.image as etai
+
+UL = 0
+BL = 1
+BR = 2
+UR = 3
+
+def is_billboard_present(corners):
+	# Return True if billboard feasible, return False otherwise
+	return True
+
+
+def angles_within_tolerance(corners, tolerance):
+	return True
