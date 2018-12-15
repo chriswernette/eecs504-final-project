3246b88e2127b28fbc986deaa355be33f922464a
Chris Wernette <chriswer@umich.edu>
2018-12-15 12:43:55 -0500

    changed tabs to spaces, fighting Python indentation

--- modules/reject.py
+++ modules/reject.py
@@ -15,36 +15,36 @@ y = 1
 
 def is_billboard_present(corners):
 	# Return True if billboard feasible, return False otherwise
-	if len(corners) != 4:
-		return False
-	tolerated = angles_within_tol(corners,horz_tol=30,vert_tol=30)
-	tolerated *= edge_lens_within_tol(corners,sim_ratio=[0.8,1.2],h_v_ratio=[2,3])
-	return tolerated
+    if len(corners) != 4:
+        return False
+    tolerated = angles_within_tol(corners,horz_tol=30,vert_tol=30)
+    tolerated *= edge_lens_within_tol(corners,sim_ratio=[0.8,1.2],h_v_ratio=[2,3])
+    return tolerated
 
 
 def angles_within_tol(cor, horz_tol, vert_tol):
-	tolerated = True
-	tolerated *= ((90 - cor_angle(cor[UL],cor[BL])) <= vert_tol)
-	tolerated *= ((90 - cor_angle(cor[BR],cor[UR])) <= vert_tol)
-	tolerated *= (cor_angle(cor[BL],cor[BR]) <= horz_tol)
-	tolerated *= (cor_angle(cor[UR],cor[UL]) <= horz_tol)
-	return tolerated
+    tolerated = True
+    tolerated *= ((90 - cor_angle(cor[UL],cor[BL])) <= vert_tol)
+    tolerated *= ((90 - cor_angle(cor[BR],cor[UR])) <= vert_tol)
+    tolerated *= (cor_angle(cor[BL],cor[BR]) <= horz_tol)
+    tolerated *= (cor_angle(cor[UR],cor[UL]) <= horz_tol)
+    return tolerated
 
 def cor_angle(c1,c2):
-	return math.degrees(math.atan2(abs(c1[y]-c2[y]), abs(c1[x]-c2[x])))
+    return math.degrees(math.atan2(abs(c1[y]-c2[y]), abs(c1[x]-c2[x])))
 
 def edge_lens_within_tol(cor, sim_ratio, h_v_ratio):
-	tolerated = True
+    tolerated = True
 
-	left   = np.linalg.norm(cor[UL] - cor[BL])
-	right  = np.linalg.norm(cor[UR] - cor[BR])
-	upper  = np.linalg.norm(cor[UL] - cor[UR])
-	bottom = np.linalg.norm(cor[BL] - cor[BR])
-	
-	tolerated *= (sim_ratio[0] <= left/right)   *   (left/right <= sim_ratio[1])
-	tolerated *= (sim_ratio[0] <= upper/bottom) * (upper/bottom <= sim_ratio[1])
-	
-	tolerated *= (h_v_ratio[0] <= upper/left)   *   (upper/left <= h_v_ratio[1])
-	tolerated *= (h_v_ratio[0] <= bottom/right) * (bottom/right <= h_v_ratio[1])
+    left   = np.linalg.norm(cor[UL] - cor[BL])
+    right  = np.linalg.norm(cor[UR] - cor[BR])
+    upper  = np.linalg.norm(cor[UL] - cor[UR])
+    bottom = np.linalg.norm(cor[BL] - cor[BR])
 
-	return tolerated
+    tolerated *= (sim_ratio[0] <= left/right)   *   (left/right <= sim_ratio[1])
+    tolerated *= (sim_ratio[0] <= upper/bottom) * (upper/bottom <= sim_ratio[1])
+
+    tolerated *= (h_v_ratio[0] <= upper/left)   *   (upper/left <= h_v_ratio[1])
+    tolerated *= (h_v_ratio[0] <= bottom/right) * (bottom/right <= h_v_ratio[1])
+
+    return tolerated
